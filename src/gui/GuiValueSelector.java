3a38170031c6fb58602c73898889e90282890dd8
oniX0n <oniX0nlp@gmail.com>
2018-05-12 12:07:51 +0200

    Started building basic gui in form.

--- src/gui/GuiValueSelector.java
+++ src/gui/GuiValueSelector.java
@@ -1,4 +1,15 @@
 package gui;
 
+import javax.swing.*;
+
 public class GuiValueSelector {
+    private JButton buttonConfrim;
+    private JPanel panelMain;
+    private JPanel panelValueSelector;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
 }
