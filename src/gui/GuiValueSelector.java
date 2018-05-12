1cec2d36b923a60927e14b693b08330976df5547
oniX0n <oniX0nlp@gmail.com>
2018-05-12 12:09:21 +0200

    Renamed all objects in gui class and form.

--- src/gui/GuiValueSelector.java
+++ src/gui/GuiValueSelector.java
@@ -6,10 +6,10 @@ public class GuiValueSelector {
     private JButton buttonConfrim;
     private JPanel panelMain;
     private JPanel panelValueSelector;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JButton buttonSelectFile;
+    private JButton buttonSelectPath;
+    private JButton buttonNumber;
+    private JTextField textFieldSelectedFile;
+    private JTextField textFieldSelectedPath;
+    private JTextField textFieldSelectNumber;
 }
