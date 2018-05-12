1931646e2d2c224ca580be733c057aff63674b0f
oniX0n <oniX0nlp@gmail.com>
2018-05-12 12:17:21 +0200

    Added button listeners and system look and feel.

--- src/gui/GuiValueSelector.java
+++ src/gui/GuiValueSelector.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiValueSelector {
     private JButton buttonConfrim;
@@ -12,4 +14,38 @@ public class GuiValueSelector {
     private JTextField textFieldSelectedFile;
     private JTextField textFieldSelectedPath;
     private JTextField textFieldSelectNumber;
+
+    public GuiValueSelector() {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        buttonSelectFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buttonSelectPath.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buttonNumber.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buttonConfrim.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
